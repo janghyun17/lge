6f9fb54cdfbb13d3b5a6b0c38d20a3b6f4de7531
janghyun <jhicbw@gmail.com>
2021-06-08 13:12:33 +0900

    21.06.08

--- images/washing-machines/md_new/usp/md_new.js
+++ images/washing-machines/md_new/usp/md_new.js
@@ -37,12 +37,12 @@ function slideStepSlider() {
 	};
 	slideStep = new Swiper($target.get(0), slideOption);
 	
-	if ($target.find('.slide_six_motion').length){
-		slideStep.on('slideChange', function (e, idx) {
+	if($target.find('.slide_six_motion').length){
+		slideStep.on('slideChange', function(e, idx){
 			$target.find('.swiper-wrapper > .swiper-slide').find('.slide_six_motion').each(function(params){
 				slideSixMotion.autoplay.stop();
 			});
-			$target.find('.swiper-wrapper > .swiper-slide').eq(slideStep.activeIndex).find('.slide_six_motion').each(function() {	
+			$target.find('.swiper-wrapper > .swiper-slide').eq(slideStep.activeIndex).find('.slide_six_motion').each(function(){	
 				slideSixMotion.autoplay.start();
 				slideSixMotion.slideTo(0);
 				$('.slide_six_motion .currentActive video').get(0).currentTime = 0;
@@ -51,7 +51,9 @@ function slideStepSlider() {
 		});
 	}
 }
-function slideSixMotionSlider() {
+
+/* 6모션 Loop */
+function slideSixMotionSlider(){
 	var $target = $('.washing_machines_202106 .slide_six_motion .swiper-container');
 	var slideOption = {
 		effect :'fade', // 페이드 효과 사용
@@ -69,32 +71,18 @@ function slideSixMotionSlider() {
 				$('.slide_six_motion_pagn a').removeClass('on');
 				$('.slide_six_motion_pagn a').eq(idx).addClass('on');
 			},
-		},
-		//observer:true,
-		//observeParents:true,
+		}
 	};
 	slideSixMotion = new Swiper($target.get(0), slideOption);
 	slideSixMotion.autoplay.stop();
-	
-	if ($target.find('video').length){
-		slideSixMotion.on('slideChange', function (e, idx) {
+	if($target.find('video').length){
+		slideSixMotion.on('slideChange', function (e, idx){
 			$target.find('.swiper-slide').find('video').each(function(params){
 				$(this).get(0).currentTime = 0;
 				$(this).get(0).pause();
 			});
 			$target.find('.swiper-slide').eq(slideSixMotion.activeIndex).find('video').each(function(){
-				// Show loading animation.
-				var playPromise = $(this).get(0).play();
-				if (playPromise !== undefined) {
-					playPromise.then(_ => {
-						$(this).get(0).play();
-					})
-					.catch(error => {
-						$(this).get(0).currentTime = 0;
-						$(this).get(0).pause();
-					});
-				}
-				console.log('st')
+				$(this).get(0).play();
 			});
 		});
 	}
@@ -102,7 +90,7 @@ function slideSixMotionSlider() {
 
 
 /** Swiper : 날씨 **/
-function slideWeatherSlider() {
+function slideWeatherSlider(){
 	var $target = $('.washing_machines_202106 .slide_weather .swiper-container');
 	var slideOption = {
 		effect : 'fade', // 페이드 효과 사용
@@ -127,7 +115,7 @@ function slideWeatherSlider() {
 				$('.slide_weather .swiper-container').removeClass('s1').removeClass('s2').removeClass('s3');
 				$('.slide_weather .swiper-container').addClass('s'+idx+'');
 			},
-		},
+		}
 	};
 	slideWeather = new Swiper($target.get(0), slideOption);
 }
@@ -135,23 +123,23 @@ function slideWeatherSlider() {
 
 /* count */
 function countNumAni(countNum){
-	Number.isInteger = Number.isInteger || function (value) {
+	Number.isInteger = Number.isInteger || function(value){
 		return typeof value == "number" && isFinite(value) && Math.floor(value) == value;
 	}
-	countNum.map(function (num) {
+	countNum.map(function(num){
 		$({ val : num.first }).animate({ val : num.last }, {
-			duration: num.duration,
-			step: function() {
-				if (Number.isInteger(num.last) == false) {
+			duration:num.duration,
+			step:function(){
+				if(Number.isInteger(num.last) == false){
 					num.$el.html(this.val.toFixed(1));
-				} else {
+				}else{
 					num.$el.html(this.val.toFixed(0));
 				}
 			},
-			complete: function() {
-				if (Number.isInteger(num.last) == false) {
+			complete:function(){
+				if(Number.isInteger(num.last) == false){
 					num.$el.html(this.val.toFixed(1));
-				} else {
+				}else{
 					num.$el.html(this.val.toFixed(0));
 				}
 			}
@@ -198,18 +186,19 @@ $(function(){
 
 /* Scroll Event */
 $(window).on('scroll', feScrollFn);
-$.fn.feScrollGet = function() {
+$.fn.feScrollGet = function(){
     var offset = $(window).scrollTop() + $(window).height() * 0.9;
 	var offset_half = $(window).scrollTop() + $(window).height() * 0.2;
+	var offset_half2 = $(window).scrollTop() + $(window).height() * 0.5;
   	
 	$animate = $('.animate');
-    $animate.each(function(i) {
+    $animate.each(function(i){
         var $ani = $(this),
             ani = $ani,
             item_top = $ani.offset().top,
             item_h = $ani.height();
-		if (($ani.offset().top) < offset) {
-            if (!$ani.hasClass('active')) {
+		if(($ani.offset().top) < offset){
+            if(!$ani.hasClass('active')){
                 $ani.addClass('active');	
             }
 			if(!$('.slide_step').hasClass('active')){
@@ -224,59 +213,58 @@ $.fn.feScrollGet = function() {
 				]
 				countNumAni(numbers);
 			}	
-        } else {
-            if ($ani.hasClass('active')) {
-               $ani.removeClass('active');
+        }else{
+            if($ani.hasClass('active')){
+            	$ani.removeClass('active');
             }
         }
     });
 	
 	
 	$videos = $('.img_video');
-
-    $videos.each(function(i) {
+    $videos.each(function(i){
         var $video = $(this).find('video'),
             video = $video,
             item_top = $video.offset().top,
             item_h = $video.height();
-        //if (($video.offset().top) < offset_half && (item_top + item_h) > offset_half) {
-		if (($video.offset().top) < (offset) && (item_top + item_h) > (offset_half)) {
-            if (!$video.hasClass('video_on')) {
+		if(($video.offset().top) < (offset) && (item_top + item_h) > (offset_half)){
+            if(!$video.hasClass('video_on')){
                 video.get(0).play();
                 $video.addClass('video_on');
             }
-        } else {
-            if ($video.hasClass('video_on')) {
+        }else{
+            if($video.hasClass('video_on')){
                 video.get(0).pause();
 				video.get(0).currentTime = 0;
                 $video.removeClass('video_on');
             }
         }	
     });
-    $('.slide_six_motion').each(function(i) {
+
+    $('.slide_six_motion').each(function(i){
         var $video2 = $(this),
             video2 = $video2,
             item_top = $video2.offset().top,
             item_h = $video2.height();
 		
-			if (($video2.offset().top) < (offset) && (item_top + item_h) > (offset_half)) {
-				if (!$video2.hasClass('video_on')) {
+		if(($video2.offset().top) < (offset) && (item_top + item_h) > (offset_half2)){
+			if(!$video2.hasClass('mot_on')){
+				if($('.slide_step').find('.swiper-slide.c4').hasClass('swiper-slide-active')){
 					slideSixMotion.autoplay.start();
-					$video2.addClass('video_on');
-					
+					$video2.addClass('mot_on');	
 				}
-			} else {
-				if ($video2.hasClass('video_on')) {
-					slideSixMotion.autoplay.stop();
-					$video2.removeClass('video_on');
-				}
-			}	
+			}
+		}else{
+			if($video2.hasClass('mot_on')){
+				slideSixMotion.autoplay.stop();
+				$video2.removeClass('mot_on');
+			}
+		}	
     });
-
 };
 
 // Scroll Event Function 
-function feScrollFn() {
+function feScrollFn(){
     $.fn.feScrollGet();
 }
 
