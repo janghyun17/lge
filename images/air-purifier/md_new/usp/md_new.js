30d1c9cba3ca8a64b157e39718ae87f999de13c7
janghyun <jhicbw@gmail.com>
2021-06-16 17:08:58 +0900

    21.06.16

---
+++ images/air-purifier/md_new/usp/md_new.js
@@ -0,0 +1,91 @@
+/*
+    공기청정기 : 21.07
+*/
+
+
+
+$(function(){
+	/* Swiper */
+
+	/* 페이지 내 스크롤 앵커  */
+    $('.air-purifier_202107 .btn_go_info').click(function(e){
+        e.preventDefault();
+		var t = $(this).attr('href');
+        if ($(t).length){
+            $('html, body').stop().animate({
+                scrollTop:$(t).offset().top - 158
+            }, 800, function(){
+                $(t).get(0).focus({preventScroll:false});
+                $('html,body').scrollTop($(t).offset().top - 158);
+            });
+        }
+    });
+    $('.air-purifier_202107 .btn_anchor a').click(function(e){
+        e.preventDefault();
+		var t = $(this).attr('href');
+        if ($(t).length){
+            $('html, body').stop().animate({
+                scrollTop:$(t).offset().top - 142
+            }, 800, function(){
+                $(t).get(0).focus({preventScroll:false});
+                $('html,body').scrollTop($(t).offset().top - 142);
+            });
+        }
+    });
+});
+
+
+
+/* Scroll Event */
+$(window).on('scroll', feScrollFn);
+$.fn.feScrollGet = function() {
+    var offset = $(window).scrollTop() + $(window).height() * 0.9;
+	var offset_half = $(window).scrollTop() + $(window).height() * 0.2;
+	var offset_half2 = $(window).scrollTop() + $(window).height() * 0.5;
+  	
+	$animate = $('.animate');
+    $animate.each(function(i){
+        var $ani = $(this),
+            ani = $ani,
+            item_top = $ani.offset().top,
+            item_h = $ani.height();
+		if (($ani.offset().top) < offset){
+            if(!$ani.hasClass('active')){
+                $ani.addClass('active');	
+            }
+        }else{
+            if($ani.hasClass('active')){
+               $ani.removeClass('active');
+            }
+        }
+    });
+
+	$videos = $('.img_video');
+    $videos.each(function(i) {
+        var $video = $(this).find('video'),
+            video = $video,
+            item_top = $video.offset().top,
+            item_h = $video.height();
+		if(($video.offset().top) < (offset) && (item_top + item_h) > (offset_half)){
+            if (!$video.hasClass('video_on')){
+                video.get(0).play();
+                $video.addClass('video_on');
+            }
+        }else{
+            if($video.hasClass('video_on')){
+                video.get(0).pause();
+				video.get(0).currentTime = 0;
+                $video.removeClass('video_on');
+            }
+        }	
+    });
+	
+};
+
+// Scroll Event Function 
+function feScrollFn() {
+    $.fn.feScrollGet();
+}
+
+
+
